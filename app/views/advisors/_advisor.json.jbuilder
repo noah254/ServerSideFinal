4c43d90727c51632560d167c02cddeb31e62306b
EC2 Default User <ec2-user@ip-172-31-42-194.ec2.internal>
2018-12-12 00:43:44 +0000

    adding database

---
+++ app/views/advisors/_advisor.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! advisor, :id, :advisorid, :firstname, :lastname, :email, :student_id, :created_at, :updated_at
+json.url advisor_url(advisor, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :students
+  resources :advisors
+  root 'students#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
